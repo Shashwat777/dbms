255a000c0f5e6809659a6267982fb3b653a88215
Shashwat777 <40899635+Shashwat777@users.noreply.github.com>
2019-02-27 00:22:21 +0530

    Update passenger.java

--- passenger.java
+++ passenger.java
@@ -11,8 +11,7 @@ public class passenger {
     ArrayList<Thread> Locktable=new ArrayList<Thread>() ;
 
 =======
-    ArrayList<Thread> LockTable=new ArrayList<Thread>();
->>>>>>> 9be3f247b73d7962699a6a26e858c5f2d9b1f18f
+    
     int id;
     passenger(){
    slock=false ;
